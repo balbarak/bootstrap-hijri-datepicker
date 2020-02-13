d685f371acc83ca496854020b84f59d02bb9efec
balbarak <balbarak@live.com>
2020-02-13 16:41:17 +0300

    update bundle config

--- dist/js/bootstrap-hijri-datepicker.js
+++ dist/js/bootstrap-hijri-datepicker.js
@@ -13847,10 +13847,12 @@
             }
             $.extend(true, options, newOptions);
             $.each(options, function (key, value) {
+
                 if (picker[key] !== undefined) {
                     picker[key](value);
                 } else {
-                    throw new TypeError('option ' + key + ' is not recognized!');
+
+                    //throw new TypeError('option ' + key + ' is not recognized!');
                 }
             });
             return picker;
